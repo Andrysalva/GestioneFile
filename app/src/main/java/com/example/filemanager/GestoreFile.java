a1379b3258d52453edc08890eaa14cdeae6991d8
Andrysalva <a.salvatori38@gmail.com>
2022-02-18 16:14:29 +0100

    Aggiunto nuovo metodo di lettura del file da resorces

--- app/src/main/java/com/example/filemanager/GestoreFile.java
+++ app/src/main/java/com/example/filemanager/GestoreFile.java
@@ -1,6 +1,7 @@
 package com.example.filemanager;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -8,6 +9,7 @@ import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
@@ -72,6 +74,24 @@ public class GestoreFile {
             esito = "errore generico nella lettura";
         }
         return esito;
+    }
+
+    public String leggiRawFile(String nf, Context c){
+        Resources res = c.getResources();
+        InputStream is= res.openRawResource(R.raw.filedaleggere);
+        StringBuilder sb = new StringBuilder();
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String str = "";
+            while((str = reader.readLine())  != null ){
+                sb.append(str + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("gestione","file inesistente");
+        } catch (IOException e) {
+            Log.e("gestione","errore nella lettura");
+        }
+        return sb.toString();
     }
 
     public String getNomeFile() {
--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_leggi;
+    Button btn_leggi_raw;
     Button btn_scrivi;
     Button btn_scrivi_bf;
     TextView testo;
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         btn_leggi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String stringaricevuta = gf.readFile("fileDaLeggere.txt",getApplicationContext());
+                String stringaricevuta = gf.readFile("filedaleggere.txt",getApplicationContext());
                 testo.setText(stringaricevuta);
                 Toast.makeText(getApplicationContext(),"letto con successo", Toast.LENGTH_LONG).show();
             }
@@ -33,18 +34,27 @@ public class MainActivity extends AppCompatActivity {
         btn_scrivi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), gf.scriviFile("fileDaLeggere.txt",text.getText().toString(),getApplicationContext()), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), gf.scriviFile("filedaleggere.txt",text.getText().toString(),getApplicationContext()), Toast.LENGTH_LONG).show();
             }
         });
         btn_scrivi_bf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), gf.scriviFileBuffered("fileDaLeggere.txt",text.getText().toString(),getApplicationContext()), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), gf.scriviFileBuffered("filedaleggere.txt",text.getText().toString(),getApplicationContext()), Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_leggi_raw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringaricevuta = gf.leggiRawFile("filedaleggere.txt",getApplicationContext());
+                testo.setText(stringaricevuta);
+                Toast.makeText(getApplicationContext(),"letto con successo", Toast.LENGTH_LONG).show();
             }
         });
     }
     public void init(){
         btn_leggi=(Button) findViewById(R.id.btn_leggi);
+        btn_leggi_raw=(Button) findViewById(R.id.btn_leggi_raw);
         btn_scrivi=(Button) findViewById(R.id.btn_scrivi);
         btn_scrivi_bf=(Button) findViewById(R.id.btn_scrivi_bf);
         testo=(TextView) findViewById(R.id.testo);
